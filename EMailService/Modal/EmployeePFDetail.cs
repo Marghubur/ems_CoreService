ab41ac1d1ecdec417fb340a024839e51fe12c171
Marghub <marghub12@gmail.com>
2024-01-25 10:42:30 +0530

    issue fixed

--- EMailService/Modal/EmployeePFDetail.cs
+++ EMailService/Modal/EmployeePFDetail.cs
@@ -7,7 +7,7 @@ namespace EMailService.Modal
     {
         public long EmployeePfDetailId { get; set; }
         public long EmployeeId { get; set; }
-        public int PFNumber { get; set; }
+        public string PFNumber { get; set; }
         public string UniversalAccountNumber { get; set; }
         public DateTime? PFJoinDate { get; set; }
         public bool IsPFEnable { get; set; }
