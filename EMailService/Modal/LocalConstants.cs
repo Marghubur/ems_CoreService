ff4d051f773f3479e47edabafd152fb5c2b994e5
BottomHalf <istiyaq.mi9@gmail.com>
2024-04-16 22:12:04 +0530

    improved formula

--- EMailService/Modal/LocalConstants.cs
+++ EMailService/Modal/LocalConstants.cs
@@ -4,5 +4,12 @@
     {
         public static string DailyJobsManager = "daily-jobs-manager";
         public static string SendEmail = "attendance_request_action";
+
+        // -----------------  Salary components constant values
+
+        public static string EPF = "EPF";
+        public static string EEPF = "EPER-PF";
+        public static string SPA = "SPA";
+        public static string ECI = "ECI";
     }
 }
