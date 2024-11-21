0cdb934ae8a934bc09ac2ee800a2fbaea88b08be
Marghubur <marghub12@gmail.com>
2024-11-21 20:28:38 +0530

    Fix: Monthly attendance and Daily Biometric attendance issue has been fixed

--- ServiceLayer/Interface/IComponentsCalculationService.cs
+++ ServiceLayer/Interface/IComponentsCalculationService.cs
@@ -11,12 +11,12 @@ namespace ServiceLayer.Interface
         decimal ProfessionalTaxComponent(EmployeeCalculation empCal, List<PTaxSlab> pTaxSlabs, int totalMonths);
         void TaxRegimeCalculation(EmployeeDeclaration employeeDeclaration, List<TaxRegime> taxRegimeSlabs, List<SurChargeSlab> surChargeSlabs);
         void NewTaxRegimeCalculation(EmployeeCalculation eCal, List<TaxRegime> taxRegimeSlabs, List<SurChargeSlab> surChargeSlabs);
-        void HRAComponent(EmployeeDeclaration employeeDeclaration, List<CalculatedSalaryBreakupDetail> calculatedSalaryBreakupDetails);
+        //void HRAComponent(EmployeeDeclaration employeeDeclaration, List<CalculatedSalaryBreakupDetail> calculatedSalaryBreakupDetails);
         void BuildTaxDetail(long EmployeeId, EmployeeDeclaration employeeDeclaration, EmployeeSalaryDetail salaryBreakup);
         decimal Get_80C_DeclaredAmount(EmployeeDeclaration employeeDeclaration);
         decimal OtherDeclarationComponent(EmployeeDeclaration employeeDeclaration);
         decimal TaxSavingComponent(EmployeeDeclaration employeeDeclaration);
         decimal HousePropertyComponent(EmployeeDeclaration employeeDeclaration);
-        decimal HRACalculation(EmployeeDeclaration employeeDeclaration, List<CalculatedSalaryBreakupDetail> calculatedSalaryBreakupDetails, int totalMonths);
+        //decimal HRACalculation(EmployeeDeclaration employeeDeclaration, List<CalculatedSalaryBreakupDetail> calculatedSalaryBreakupDetails, int totalMonths);
     }
 }
