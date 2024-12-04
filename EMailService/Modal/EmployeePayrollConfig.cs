7650474d953eea3ed5cdd74629d126c32e321d7e
Marghubur <marghub12@gmail.com>
2024-12-04 18:24:11 +0530

    some error fixed

--- EMailService/Modal/EmployeePayrollConfig.cs
+++ EMailService/Modal/EmployeePayrollConfig.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace EMailService.Modal
 {
-    public class EmployeePFDetail: ManagerDetail
+    public class EmployeePayrollConfig: ManagerDetail
     {
         public long EmployeePfDetailId { get; set; }
         public long EmployeeId { get; set; }
         public string PFNumber { get; set; }
-        public string UniversalAccountNumber { get; set; }
-        public DateTime? PFJoinDate { get; set; }
+        public string UAN { get; set; }
+        public DateTime? PFAccountCreationDate { get; set; }
         public bool IsPFEnable { get; set; }
     }
 }
